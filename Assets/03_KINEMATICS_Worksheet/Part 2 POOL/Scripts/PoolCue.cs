60fede1996376041b2ed95130844b3aef7f19d61
TeoEnKi <teoenki@gmail.com>
2023-11-15 21:40:39 +0800

    Kinematics worksheet completed
    However I think the code is wrong eventhough the scene is running smoothly

---
+++ Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/PoolCue.cs
@@ -0,0 +1,61 @@
+using NUnit;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public class PoolCue : MonoBehaviour
+{
+    public LineFactory lineFactory;
+    public GameObject ballObject;
+
+    private Line drawnLine;
+    private Ball2D ball;
+
+    private void Start()
+    {
+        ball = ballObject.GetComponent<Ball2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var startLinePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Start line drawing
+            if(ball != null && ball.IsCollidingWith(startLinePos.x,startLinePos.y))
+            {
+                drawnLine = lineFactory.GetLine(new Vector2(ball.Position.x, ball.Position.y), startLinePos, 1f, UnityEngine.Color.black);
+                drawnLine.EnableDrawing(true);
+            }
+        }
+        else if (Input.GetMouseButtonUp(0) && drawnLine != null)
+        {
+            drawnLine.EnableDrawing(false);
+
+            //update the velocity of the white ball.
+            HVector2D v = new HVector2D(Camera.main.ScreenToWorldPoint(Input.mousePosition).x - ball.Position.x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y - ball.Position.y);
+            ball.Velocity = v;
+
+            drawnLine = null; // End line drawing            
+        }
+
+        //when dragging the mouse is outside the ball
+        if (drawnLine != null)
+        {
+            drawnLine.end = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Update line end
+        }
+    }
+
+    /// <summary>
+    /// Get a list of active lines and deactivates them.
+    /// </summary>
+    public void Clear()
+    {
+        var activeLines = lineFactory.GetActive();
+
+        foreach (var line in activeLines)
+        {
+            line.gameObject.SetActive(false);
+        }
+    }
+}
