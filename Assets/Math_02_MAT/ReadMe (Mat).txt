058eaf018379e73870f30023e0ed1e88a34dee44
TeoEnKi <teoenki@gmail.com>
2023-12-07 22:31:53 +0800

    renaming Math folder

---
+++ Assets/Math_02_MAT/ReadMe (Mat).txt
@@ -0,0 +1,7 @@
+Placeholder "Type of Matrix" for 1st Mat input: Only "3x3"
+Placeholder "Type of Matrix" for 2nd Mat input: either "3x1" or "3x3"
+
+Getting result Mat: press enter
+
+
+If the input of the 2nd matrix is 3x1, do not need to input anything for m20, m01, m02, m11, m12, m21, m22 in 2nd matrix
--- Assets/Math_02_MAT/TestMatrix.cs
+++ Assets/Math_02_MAT/TestMatrix.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class TestMatrix : MonoBehaviour
 {
@@ -33,7 +34,7 @@ public class TestMatrix : MonoBehaviour
     void Update()
     {
         //input the values
-        //run q2 after pressingenter
+        //run q2 after pressing enter
         if (Input.GetKeyDown(KeyCode.Return))
         {
             CreateMatrixArray();
@@ -44,6 +45,7 @@ public class TestMatrix : MonoBehaviour
 
     public void Question2()
     {
+        //if it is matrix x matrix, print the array of array of elements of the result matrix
         string grid = "";
         if (IsMatMat())
         {
@@ -53,15 +55,19 @@ public class TestMatrix : MonoBehaviour
             {
                 for (int x = 0; x < 3; x++)
                 {
-                    grid = grid + resultMat.Entries[y, x] + " ";
+                    //print element along with spacing 
+                    grid = grid + resultMat.Entries[y, x] + "   ";
                 }
+                //for every new row, create a new line
                 grid += "\n";
 
             }
         }
         else
         {
+            //if it is matrix x vector, print the array of array of elements of the result vector
             resultVec = mats[0] * vec1;
+            //new row between elements
             grid = resultVec.x + "\n" + resultVec.y + "\n" + resultVec.h;
         }
         result.text = grid;
@@ -70,6 +76,7 @@ public class TestMatrix : MonoBehaviour
 
     private bool IsMatMat()
     {
+        //determine if the equation is matrix x matrix or matrix x vector using the "type of matrix" inputs
         TMP_InputField[] matsUsed = FindObjectsOfType<TMP_InputField>().Where(t => t.transform.name == "type of matrix").ToArray();
         foreach (TMP_InputField mat in matsUsed)
         {
@@ -88,24 +95,31 @@ public class TestMatrix : MonoBehaviour
             string typeOfMatrix = inputMatrices[i].transform.Find("type of matrix").GetComponent<TMP_InputField>().text;
             if (typeOfMatrix == "3x3")
             {
-                //get object in list
+                //clear array of array (matrix)
+                Array.Clear(tMatInput, 0, tMatInput.Length);
+
+                //get input objects that are children on type of matrix object
                 foreach (Transform child in inputMatrices[i].transform)
                 {
                     if (child.name != "type of matrix")
                     {
+                        //get the placeholder name
                         char[] pos = child.name.Replace("m", "").ToCharArray();
 
+                        //the 1st digit is the row while the second digit is the column
                         int row = (int)char.GetNumericValue(pos[0]);
                         int col = (int)char.GetNumericValue(pos[1]);
-                        Debug.Log(row + " and " + col + " / " + pos[0] + " and " + pos[1]);
 
+                        //put that input of in the array of array based on the position (row and column from input object name)
                         tMatInput[row, col] = int.Parse(child.GetComponent<TMP_InputField>().text);
                     }
                 }
+                //add the 1 or 2 matrix inputs in a matrix array
                 mats[i] = new HMatrix2D(tMatInput);
             }
             else if (typeOfMatrix == "3x1")
             {
+                //get the inputs from input objects with the m00 and m01 placeholders if the type of matrix input in a vector
                 tVecInput.x = int.Parse(inputMatrices[i].transform.Find("m00").GetComponent<TMP_InputField>().text);
                 tVecInput.y = int.Parse(inputMatrices[i].transform.Find("m10").GetComponent<TMP_InputField>().text);
                 vec1 = new HVector2D(tVecInput);
